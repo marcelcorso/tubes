6710e191efeac15072edf025bf3c51a6d8cc7c72
Marcel Corso <marcel.corso@gmail.com>
2012-12-10 20:29:21 +0100

    finally mario has work again. thanks obama

--- tubes.go
+++ tubes.go
@@ -8,16 +8,22 @@ import (
 )
 
 type Tube struct {
-  channel chan string
+  in chan string
+  out chan string
   subscribers *list.List
   name string
 }
 
 func newTube(name string) *Tube {
   t := new(Tube)
-  t.channel = make(chan string, 8)
+  t.in = make(chan string, 8)
+  t.out = make(chan string, 8)
   t.subscribers = list.New()
   t.name = name
+
+  // takes from 'in' and put on all subscriber's 'out'
+  go t.Multiplier()
+
   return t
 }
 
@@ -25,34 +31,40 @@ func (t *Tube) Sub(target *Tube) {
   target.subscribers.PushBack(t)
 }
 
-func (t *Tube) Pub(s string) {
-  fmt.Println("pub!")
-  // send on this tube's channel
-  t.channel <- s
-
-  for e := t.subscribers.Front(); e != nil; e = e.Next() {
-    fmt.Println("s?")
-    t2 := e.Value.(*Tube)
-    fmt.Println(t2.name)
-    // send on the tube's subscribed channel
-    t2.channel <- s
+// forever running multiplier goroutine
+func (t *Tube) Multiplier() {
+  for {
+    // take from the in channel
+    s := <-t.in
+    // send on the out channel
+    t.out <- s
+    // and on the subscriber's in channel
+    for e := t.subscribers.Front(); e != nil; e = e.Next() {
+      t2 := e.Value.(*Tube)
+      // send on the tube's subscribed channels
+      t2.in <- s
+    }
   }
 }
 
 func main() {
   fmt.Println("你好, 世界")
 
+  // luigi brings the tubes
   t1 := newTube("t1")
   t2 := newTube("t2")
+  t3 := newTube("t3")
 
+  // mario conects them
   t2.Sub(t1)
+  t3.Sub(t2)
 
-  t1.Pub("bracatinga")
-  t2.Pub("pirarucu")
-  t1.Pub("capivara")
-  t2.Pub("ipe")
+  // por favor no hechar el papel en el baño. (taza)
+  t1.in <- "bracatinga"
+  t2.in <- "pirarucu"
+  t3.in <- "capivara"
 
-  go out(t2)
+  go out(t3)
 
   time.Sleep(1 * 1e9)
   fmt.Println("再见！")
@@ -62,7 +74,7 @@ func main() {
 
 func out(t *Tube) {
   for i := 0; i < 4; i++ {
-    fmt.Println(<-t.channel)
+    fmt.Println(<-t.out)
   }
 }
 
